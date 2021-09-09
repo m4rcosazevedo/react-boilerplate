2f83c4dc2f8a2c1dd44608236528a6b1a45552c0
Marcos Azevedo <marcos.workspace@gmail.com>
2021-09-08 23:10:33 -0300

    chore: add axios class implements

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,19 @@ module.exports = {
   ],
   rules: {
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error']
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    camelcase: 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-useless-constructor': 'off',
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': [
+      'error',
+      {
+        ignoreDeclarationMerge: true
+      }
+    ]
   }
 }
